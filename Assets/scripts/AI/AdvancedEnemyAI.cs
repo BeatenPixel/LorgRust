a81154695944de0079c991c642cde663ab388f2c
ilya_stasik <stasik@plavalaguna.ru>
2020-10-20 15:40:16 +0300

    Lesson5

---
+++ Assets/scripts/AI/AdvancedEnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AdvancedEnemyAI : MonoBehaviour {
+    public int health = 100;
+    public float viewRange = 25f;
+    public float attackRange = 5f;
+
+    public bool isChasing = false;
+
+    private NavMeshAgent agent;
+    private Transform playerTransform;
+
+    private void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        Ray ray = new Ray(transform.position, Vector3.forward);
+        RaycastHit hitInfo;
+
+        CheckHealth();
+
+        if (Physics.Raycast(ray, out hitInfo, viewRange)) {
+
+            if (hitInfo.collider.tag == "Player") {
+                if (isChasing == false) {
+                    isChasing = true;
+                    playerTransform = hitInfo.collider.GetComponent<Transform>();
+                }
+            }
+
+        }
+
+        if (Physics.Raycast(ray, out hitInfo, attackRange)) {
+
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * viewRange);
+
+        if (isChasing == true) {
+            agent.SetDestination(playerTransform.position);
+        }
+
+    }
+
+    public void TakeDamage(int damage) {
+
+        health -= damage;
+        Debug.Log("Enemy took damage and now has" + health + " health!");
+    }
+    private void CheckHealth() {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/PLayer/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+   public GameObject deathCanvas;
+   
+    public Vector3 spawnPoint = Vector3.zero;
+    public float health = 100;
+
+    private void Update () 
+        {
+            CheckDeath();
+        }
+
+    private void Start() 
+        {
+        deathCanvas.SetActive(false);
+    }
+
+    private void CheckDeath() 
+    {
+        if (health <= 0)
+        {
+            Die();
+        }
+
+    }
+    private void Die() {
+        deathCanvas.SetActive(true);
+    } 
+    
+    public void Respawn() {
+        deathCanvas.SetActive(false);
+        transform.position = spawnPoint;
+        health = 100;
+    }
+    
+   
+}
